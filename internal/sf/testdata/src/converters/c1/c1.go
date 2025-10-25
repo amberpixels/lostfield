a39d5515f7382f697a42cca99b499aeca7a91f48
Eugene M <eugene@amberpixels.io>
2025-10-25 08:48:29 +0300

    nilderef issue?

--- internal/sf/testdata/src/converters/c1/c1.go
+++ internal/sf/testdata/src/converters/c1/c1.go
@@ -8,13 +8,14 @@ import (
 func ConvertSampleToDB(sample model.Sample) (result *dbmodel.Sample) {
 	_ = sample.Label
 	_ = sample.ID
-	_ = result.ID
 
 	result = &dbmodel.Sample{
 		Label:    "const label",
 		Currency: sample.Currency,
 	}
 	result.Price = sample.Price
+
+	_ = result.ID
 
 	return
 }
